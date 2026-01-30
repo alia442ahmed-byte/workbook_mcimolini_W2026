7486e5c12df2731d8eb68bb2b92392e0d44fc443
alia442ahmed-byte <alia442ahmed@gmail.com>
2026-01-30 10:43:57 -0500

    Match case

---
+++ Module 3/Day 8/great_match_case.py
@@ -0,0 +1,29 @@
+letter_grade = input("Enter your letter grade (A, B, C, or D): ").upper()
+# or letter_grade = letter_grade.upper()
+# This will change all of our input to upper-case for ease of comparison
+
+# Convert our letter grade to a GPA
+match letter_grade:
+    case "A":
+        gpa = 4.0
+    case "B":
+        gpa = 3.3
+    case "C":
+        gpa = 2.5
+    case "D":
+        gpa = 2.0
+    case "F":
+        gpa = 1.0
+    case _: # default case
+        print("Could not determine numeric grade")
+        gpa = 0.0
+#  Equal to:
+#  if letter_grade == "A":
+#      gpa = 4.0
+#  elif letter_grade == "B":
+#      gpa = 3.3
+#  .....
+# else:
+#    print("Could not determine numeric grade")
+
+print(f"Your GPA is {gpa}")
